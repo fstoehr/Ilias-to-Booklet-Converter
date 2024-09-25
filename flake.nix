a90f4627b56167075a2fbb09bd6cdfd05ff007d4
Fabian Stoehr <Fabian@Anaxagoras>
2024-09-26 00:06:56 +0200

    Fixed nix flake

--- flake.nix
+++ flake.nix
@@ -7,24 +7,41 @@
     flake-utils.lib.eachDefaultSystem (system: 
       let
         pkgs = import nixpkgs { inherit system; };
-        my-name = "ilias-to-booklet";
-        my-buildInputs = with pkgs; [ 
+        script-name = "ilias-to-booklet";
+        script-version = "0.1-git-20240925";
+        commands-used-in-script = with pkgs; [ 
             p7zip findutils imagemagick gnused bc pdftk ghostscript
             poppler_utils # for pdfinfo
-            # glibcLocales # to prevent ugly perl messages - probably no longer needed as we no longer use rename
             coreutils # for mv
           ];
-        ilias-to-booklet = (pkgs.writeScriptBin my-name (builtins.readFile ./ilias-to-booklet.sh)).overrideAttrs (old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
+        #ilias-to-booklet = (pkgs.writeScriptBin my-name (builtins.readFile ./ilias-to-booklet.sh)).overrideAttrs (old: {
+        #  buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        #});
       in rec {
         defaultPackage = packages.ilias-to-booklet-converter;
-        packages.ilias-to-booklet-converter = pkgs.symlinkJoin {
-          name = my-name;
-          paths = [ ilias-to-booklet ] ++ my-buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
-        };
+        packages.ilias-to-booklet-converter = pkgs.stdenv.mkDerivation {
+          src = ./.;
+          pname = script-name; # don't forget to include version number eventually
+          version = script-version;
+          inherit system;
+          nativeBuildInputs = [ pkgs.makeWrapper ]; # We don't need this at run time, so it's nativeBuildInputs rather than buildInputs.
+          unpackPhase = ''
+          cp ${./ilias-to-booklet.sh} ilias-to-booklet.sh
+          '';
+          buildPhase = ''
+          patchShebangs ilias-to-booklet.sh
+          '';
+          installPhase = ''
+          mkdir -p $out/bin
+          mv ilias-to-booklet.sh $out/bin/ilias-to-booklet
+          
+          wrapProgram $out/bin/ilias-to-booklet --set PATH ${nixpkgs.lib.makeBinPath commands-used-in-script}
+          #wrapProgram $out/bin/ilias-to-booklet --prefix PATH : ${nixpkgs.lib.makeBinPath commands-used-in-script}
+
+	  '';
+	}; 
+
       }
     );
+    #);
 }
